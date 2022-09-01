a42bda66e18b2a3628bdccafbbd088d0cfa4309e
Johnson <johnson.nganga@student.moringaschool.com>
2022-09-02 00:52:07 +0300

    project

--- Movie app/script.js
+++ Movie app/script.js
@@ -4,16 +4,16 @@ const IMGPATH = "https://image.tmdb.org/t/p/w500";
 const SEARCHAPI =
     "https://api.themoviedb.org/3/search/movie?&api_key=f6623a5a4e696bc6c4435deec3dded06&query=";
 
-const main = document.getElementById("main");
-const form = document.getElementById("form");
-const search = document.getElementById("search");
+var main = document.getElementById("main");
+var search = document.getElementById("search");
+var form = document.getElementById("form");
 
 
 getMovies(APIURL);
 
 async function getMovies(url) {
-    const resp = await fetch(url);
-    const respData = await resp.json();
+    var resp = await fetch(url);
+    var respData = await resp.json();
 
     console.log(respData);
 
@@ -25,10 +25,10 @@ function showMovies(movies) {
     main.innerHTML = "";
 
     movies.forEach((movie) => {
-        const { poster_path, title, vote_average, overview } = movie;
+        var { poster_path, title, vote_average, overview } = movie;
 
-        const movieEl = document.createElement("div");
-        movieEl.classList.add("movie");
+        var movieEl = document.createElement("div");
+        movieEl.classList.add("movies");
 
         movieEl.innerHTML = `
             <img
@@ -37,9 +37,8 @@ function showMovies(movies) {
             />
             <div class="movie-info">
                 <h3>${title}</h3>
-                <span class="${getClassByRate(
-                    vote_average
-                )}">${vote_average}</span>
+                <span class="${getClassByRate( vote_average )}">${vote_average}</span>
+                    
             </div>
             <div class="overview">
                 <h3>Overview:</h3>
@@ -50,3 +49,25 @@ function showMovies(movies) {
         main.appendChild(movieEl);
     });
 }
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return "green";
+    } else if (vote >= 5) {
+        return "orange";
+    } else {
+        return "red";
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    var searchTerm = search.value;
+
+    if (searchTerm) {
+        getMovies(SEARCHAPI + searchTerm);
+
+        search.value = "";
+    }
+});
